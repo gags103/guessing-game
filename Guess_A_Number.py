ef41b8373727ac0f651bf3c62db9ccaff3fe89df
gags103 <45313599+gags103@users.noreply.github.com>
2019-03-17 13:28:21 -0400

    First commit

---
+++ Guess_A_Number.py
@@ -0,0 +1,32 @@
+import random
+
+for x in range(1):
+	answer = random.randint(1,21)
+
+message = ''
+active = True
+while active:
+	message = input(' Play a guessing game press enter or no to quit: ')
+	if message == 'no':
+		active = False
+
+	else:
+		for value in range(1,6):
+			input_number = input(" pick a number between 1 and 20 : ")
+			number = int(input_number)
+			if answer > number:
+				print("To Low")
+			elif answer < number:
+				print("To High")
+			elif answer == number:
+				print("you are correct")
+				break
+		if answer != number:		
+			print('Sorry you only get 5 chances ')
+
+print("Thanks for playing")
+
+		
+
+
+
